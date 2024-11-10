81649420336d9052eeba60585425697665879165
kalilakorin <kalila.korin@gmail.com>
2024-11-09 19:52:05 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,16 +1,16 @@
 // Step 1 enter your mapbox studio token
-mapboxgl.accessToken = 'xxxxxx';
+mapboxgl.accessToken = 'pk.eyJ1Ijoiam1mcmltbWwiLCJhIjoiY20wZzdid3JyMTkweTJpb3J1YnJ6b3BkNiJ9.AIq8-_n3FX7v_45I0Ria3w';
 
 var map = new mapboxgl.Map({
   container: 'map',
   // Step 2 - put in your style that you generated in Lab 1 for the Boulder area
   // e.g. mapbox://styles/user/ck27u9ejp1e291cmo0c78b4e3
-  style: 'xxxxxx',
-  center:[-105.28391,40.02072],
+  style: 'mapbox://styles/jmfrimml/cm1g0660r03xz01rbav7b1hzu',
+  center:[-105.285,40.052],
   // Modify these settings as you see fit!!
   bearing: 0, // bearing in degrees
   pitch: 60, // for 3d perspective
-  zoom: 12  //zoom level
+  zoom: 16  //zoom level
 });
 
 map.on('load', function () {
@@ -23,7 +23,7 @@ map.on('load', function () {
             // for data url enter the arcgis service with format geojson (f=geojson)
             // and spatial reference 4326 (WGS84 or Geographic) 
             // e.g. replace xxxxxx values below
-            'data': 'https://services.arcgis.com/YseQBnl2jq0lrUV5/arcgis/rest/services/Intersect_of_Intersect_of_MobileHomes_and_Dissolve_Buffer_of_Wildfire_History_and_FloodRisk/FeatureServer/0/query?where=flood_risk+%3D+%27high%27&outSR=xxxxxx&f=xxxxxx'
+            'data': 'https://services.arcgis.com/YseQBnl2jq0lrUV5/arcgis/rest/services/Intersect_of_Intersect_of_MobileHomes_and_Dissolve_Buffer_of_Wildfire_History_and_FloodRisk/FeatureServer/0/query?where=flood_risk+%3D+%27high%27&outSR=4326&f=geojson'
         },
         'paint': {
           // make circles larger as the user zooms from z12 to z22
@@ -34,7 +34,7 @@ map.on('load', function () {
           // use an html color picker to enter a circle color of your choice below using 
           // hexadecimal values e.g. 
           // #000000 for black replace xxxxxx with your value
-          'circle-color': 'xxxxxx',
+          'circle-color': '#990000',
           'circle-stroke-width': 1,
           'circle-stroke-color': '#000'
         }
